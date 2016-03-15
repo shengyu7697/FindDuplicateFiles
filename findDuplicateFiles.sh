456e9997fd03acffa4fd89c8eca0ca2e73668745
shengyu <shengyu7697@gmail.com>
2016-03-15 15:45:24 +0800

    output log when using rm command

--- findDuplicateFiles.sh
+++ findDuplicateFiles.sh
@@ -53,8 +53,8 @@ function deleteDuplicateFile()
     while read line
     do
         FILEPATH=$(echo "$line" | cut -d$'\t' -f2-)
+        echo "rm -rf $FILEPATH"
         rm -rf "$FILEPATH"
-
     done < $1
 }
 
